1dd340c421d3ab6f27adf04f5c02edce522db8e4
ALLARRA <ALLARRA@ZHA-ITA055-W7.corp.oocl.com>
2017-07-31 17:01:54 +0800

    Initial build

---
+++ Day1/src/main/java/MavenDay1/Day1/App.java
@@ -0,0 +1,13 @@
+package MavenDay1.Day1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
